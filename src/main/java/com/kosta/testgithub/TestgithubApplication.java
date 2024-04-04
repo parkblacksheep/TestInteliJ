2a572b94666a34eccbc951108db97e0109d8c383
parkblacksheep <p7421772@naver.com>
2024-04-04 17:49:06 +0900

    first commit

---
+++ src/main/java/com/kosta/testgithub/TestgithubApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.testgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestgithubApplication.class, args);
+    }
+
+}
